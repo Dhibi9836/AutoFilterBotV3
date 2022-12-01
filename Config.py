c3811fd7a5b7e8881d4c6e2a8142f8259a743983
Dhibi9836 <107266932+Dhibi9836@users.noreply.github.com>
2022-12-01 20:34:33 +0530

    Ooh

--- Config.py
+++ Config.py
@@ -5,34 +5,34 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
+SESSION = environ.get('LuciferMoringstar_Robot')
 API_ID = int(environ[5530754])
 API_HASH = environ['5e51ecf5945605c711fffe7b376fa2a8']
 BOT_TOKEN = environ['5694714864:AAHBWW5y2eZ-6xyGuygZOxhLWV5CxaA40HU
 ']
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+CACHE_TIME = int(environ.get(300))
+USE_CAPTION_FILTER = bool(environ.get(False))
 
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", -1001696170170))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", 1170610542).split())
-DB_URL = os.environ.get("DATABASE_1", "")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
+BROADCAST_CHANNEL = int(os.environ.get(-1001865920944))
+ADMIN_ID = set(int(x) for x in os.environ.get(1170610542).split())
+DB_URL = os.environ.get("mongodb+srv://123:123@cluster1.7wqy6xv.mongodb.net/?retryWrites=true&w=majority")
+BROADCAST_AS_COPY = bool(os.environ.get(True))
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ[1170610542].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ[-1001594114982].split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get().split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('FORCES_SUB')
+auth_channel = environ.get(-1001696170170)
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
+AUTH_GROUPS = [int(admin) for admin in environ.get().split()]
 TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
 # MongoDB information
-DATABASE_URI = environ['DATABASE_2']
+DATABASE_URI = environ[]
 DATABASE_NAME = environ['BOT_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('Telegram_files')
 
 # Messages
 default_start_msg = """
