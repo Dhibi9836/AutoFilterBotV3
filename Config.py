22bb51874a1097d78d17859bcafd5570e94311f1
Dhibi9836 <107266932+Dhibi9836@users.noreply.github.com>
2022-12-01 20:51:50 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,52 +1,32 @@
-import re
-import os
-from os import environ
-
-id_pattern = re.compile(r'^.\d+$')
-
 # Bot information
-SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = 'Movies Bot'
+USER_SESSION = 'User_Bot'
+API_ID = 5530754
+API_HASH = '5e51ecf5945605c711fffe7b376fa2a8'
+BOT_TOKEN = '5694714864:AAHBWW5y2eZ-6xyGuygZOxhLWV5CxaA40HU
+'
+USERBOT_STRING_SESSION = 'LuciferMoringstar_Robot'
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-
-BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
-ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
-DB_URL = os.environ.get("DATABASE_1", "")
-BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('FORCES_SUB')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
-AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
-TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
+ADMINS = [1170610542 , 871386704]
+CHANNELS = [-1001199163986 , -1001639078000 , -1001594114982 , -1001396665860]
+AUTH_USERS = []
+AUTH_CHANNEL = None
+
 # MongoDB information
-DATABASE_URI = environ['DATABASE_2']
-DATABASE_NAME = environ['BOT_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://123:123@cluster1.7wqy6xv.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = 'LuciferMoringstar_Robot'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
-default_start_msg = """
-Hi, I'm Moviez Bot 4
-Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
+START_MSG = """
+**Hi, I'm Moviex Bot 4 bot**
+Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
-START_MSG = environ.get('START_MSG', default_start_msg)
 
-FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "")
-OMDB_API_KEY = environ.get("OMDB_API_KEY", "http://www.omdbapi.com/?i=tt3896198&apikey=4f08a979")
-if FILE_CAPTION.strip() == "":
-    CUSTOM_FILE_CAPTION=None
-else:
-    CUSTOM_FILE_CAPTION=FILE_CAPTION
-if OMDB_API_KEY.strip() == "":
-    API_KEY=None
-else:
-    API_KEY=OMDB_API_KEY
+SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
+INVITE_MSG = 'Please join https://t.me/moviesupdateinform to use this bot'
