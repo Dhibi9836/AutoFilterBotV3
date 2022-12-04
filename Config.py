220d95f3d31d947ea30cff332cef6aa5873357bd
Dhibi9836 <107266932+Dhibi9836@users.noreply.github.com>
2022-12-04 10:02:50 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -5,26 +5,26 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 # Bot information
-SESSION = "LuciferMoringstar_Robot"
-API_ID = 5530754
-API_HASH = "5e51ecf5945605c711fffe7b376fa2a8"
-BOT_TOKEN = "5694714864:AAHBWW5y2eZ-6xyGuygZOxhLWV5CxaA40HU"
+SESSION = environ.get('SESSION', 'LuciferMoringstar_Robot')
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
-CACHE_TIME = 300
-USE_CAPTION_FILTER = False
+CACHE_TIME = int(environ.get('CACHE_TIME', 300))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
-BROADCAST_CHANNEL = -1001865920944
-ADMIN_ID = 1170610542
-DB_URL = "mongodb+srv://123:123@cluster1.7wqy6xv.mongodb.net/?retryWrites=true&w=majority"
-BROADCAST_AS_COPY = True
+BROADCAST_CHANNEL = int(os.environ.get("BROADCAST_CHANNEL", ""))
+ADMIN_ID = set(int(x) for x in os.environ.get("ADMIN_ID", "").split())
+DB_URL = os.environ.get("DATABASE_1", "")
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST", True))
 
 # Admins, Channels & Users
-ADMINS = [1170610542]
-CHANNELS = [-1001594114982]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = 
+auth_channel = environ.get('FORCES_SUB')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 AUTH_GROUPS = [int(admin) for admin in environ.get("AUTH_GROUPS", "").split()]
 TUTORIAL = "https://youtu.be/5hnYOKBzyi8"
@@ -35,7 +35,8 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-Hi, I'm Moviez Bot 4
+**Hi, I'm Auto Filter V3**
+
 Here you can search files in Inline mode as well as PM, Use the below buttons to search files or send me the name of file to search.
 """
 START_MSG = environ.get('START_MSG', default_start_msg)
